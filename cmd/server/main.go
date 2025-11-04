0dd0f98e5cbf24c19b709501b7023ba8e1472827
karthik <karthikhs1402@gmail.com>
2025-11-04 20:10:51 +0530

    refactor(main): remove redundant code after log fatal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/brutally-Honest/distributed-rate-limiter/internal/config"
 	"github.com/brutally-Honest/distributed-rate-limiter/internal/server"
@@ -13,13 +12,11 @@ func main() {
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("App error %v", err)
-		os.Exit(1)
 	}
 
 	srv, err := server.New(cfg)
 	if err != nil {
 		log.Fatalf("Server error %v", err)
-		os.Exit(1)
 	}
 
 	if err := server.RunWithGracefulShutdown(srv); err != nil {
